fcf16e57c0ed4da576bb6a2ca0b7cb8e5acdf517
chandakumari456 <chandakumari456@gmail.com>
2016-03-03 12:04:30 +0530

    Added files via upload

---
+++ LearnJava.java
@@ -0,0 +1,13 @@
+package com.digital;
+
+public class LearnJava {
+
+	public static void main(String[] args) {
+	
+		System.out.println("first line in class");
+		System.out.println("2nd line in class");
+		
+		
+	}
+
+}
